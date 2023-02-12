245c59244bb02a4544caf8f2ea193bf86dbff2d4
alexone <persyak@ukr.net>
2023-02-12 13:30:31 +0200

    Remove unnecessary import

--- src/main/java/org/ogorodnik/shop/web/controller/LoginController.java
+++ src/main/java/org/ogorodnik/shop/web/controller/LoginController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.Optional;
 
 @Slf4j
