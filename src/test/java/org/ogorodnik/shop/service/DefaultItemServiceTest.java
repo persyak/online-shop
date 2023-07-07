f71154cab4327d9663ee8fd3a711ad4172bb585a
alexone <persyak@ukr.net>
2023-07-07 10:31:14 +0300

    Refactor DefaultItemServiceTest

--- src/test/java/org/ogorodnik/shop/service/DefaultItemServiceTest.java
+++ src/test/java/org/ogorodnik/shop/service/DefaultItemServiceTest.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class ItemServiceTest {
+class DefaultItemServiceTest {
 
     @Autowired
     private ItemService itemService;
